7c98a7032892805d92b9b6848eb713a5a8c1dea7
Karel Faille <shaffe.fr@gmail.com>
2020-01-24 00:59:20 +0100

    Add support of custom and default attributes

--- README.md
+++ README.md
@@ -18,16 +18,18 @@ TextCounted::make('Meta title')
 
 ![Image of character counter](docs/screenshot.jpg)
 
-You can use the text and textarea fields with charactercounters on you Nova model. The max number of characters aren't enforced, but just encouraged with warning colors and the counter. (You could enforce the max number of characters with Nova's built in _rules_).
+You can use the text and textarea fields with charactercounters on you Nova model. The max number of characters aren't enforced, but just encouraged with warning colors and the counter. (You could enforce the max number of characters with Nova's built in _rules_ and with a _maxlength_ extra attribute).
 
 ```php
 TextCounted::make('Meta title')
     ->maxChars(60)
-    ->warningAt(50),
+    ->warningAt(50)
+    ->withMeta(['extraAttributes' => ['maxlength' => '65']]),
 
 TextareaCounted::make('Meta description')
     ->maxChars(160)
-    ->warningAt(150),
+    ->warningAt(150)
+    ->rows(3),
 ```
 
 The maxChars and warningAt are both optional. The color of the counter will change when the max or warningAt limit is reached.
--- resources/js/components/TextCounted/FormField.vue
+++ resources/js/components/TextCounted/FormField.vue
@@ -1,13 +1,15 @@
 <template>
-    <default-field :field="field">
+    <default-field :field="field" :errors="errors">
         <template slot="field">
             <div class="relative">
                 <input
                     type="text"
-                    v-model="value"
                     class="w-full form-control form-input form-input-bordered"
-                    :class="errorClasses"
-                    :placeholder="field.name"
+                    :id="field.attribute"
+                    :readonly="readonly"
+                    :required="required"
+                    v-model="value"
+                    v-bind="extraAttributes"
                 />
 
                 <charcounter :value="value" :max-chars="field.maxChars" :warning-threshold="field.warningAt"></charcounter>
@@ -31,6 +33,28 @@
 
         components: {
             Charcounter
+        },
+
+        computed: {
+            defaultAttributes() {
+                return {
+                    pattern: this.field.pattern,
+                    placeholder: this.field.placeholder || this.field.name,
+                    class: this.errorClasses,
+                }
+            },
+
+            extraAttributes() {
+                const attrs = this.field.extraAttributes
+
+                return {
+                    // Leave the default attributes even though we can now specify
+                    // whatever attributes we like because the old number field still
+                    // uses the old field attributes
+                    ...this.defaultAttributes,
+                    ...attrs,
+                }
+            },
         }
     }
 </script>
