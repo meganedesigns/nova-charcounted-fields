2f110e8a1592cc0f346370bf33bf67f2429de4b3
Nicholas Welch <nc.welch@icloud.com>
2021-04-21 20:25:21 -0400

    prod

--- resources/js/field.js
+++ resources/js/field.js
@@ -1,9 +1,9 @@
 Nova.booting((Vue, router) => {
-    Vue.component('index-text-counted', require('./components/TextCounted/IndexField.vue'));
-    Vue.component('detail-text-counted', require('./components/TextCounted/DetailField.vue'));
-    Vue.component('form-text-counted', require('./components/TextCounted/FormField.vue'));
+    Vue.component('index-text-counted', require('./components/TextCounted/IndexField'));
+    Vue.component('detail-text-counted', require('./components/TextCounted/DetailField'));
+    Vue.component('form-text-counted', require('./components/TextCounted/FormField'));
 
-    Vue.component('index-textarea-counted', require('./components/TextareaCounted/IndexField.vue'));
-    Vue.component('detail-textarea-counted', require('./components/TextareaCounted/DetailField.vue'));
-    Vue.component('form-textarea-counted', require('./components/TextareaCounted/FormField.vue'));
+    Vue.component('index-textarea-counted', require('./components/TextareaCounted/IndexField'));
+    Vue.component('detail-textarea-counted', require('./components/TextareaCounted/DetailField'));
+    Vue.component('form-textarea-counted', require('./components/TextareaCounted/FormField'));
 })
