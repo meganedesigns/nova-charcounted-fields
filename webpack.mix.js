8787b73dc431a60b0dab9820a6d249d37c48d118
Nicholas Welch <nc.welch@icloud.com>
2021-04-21 20:27:53 -0400

    prod

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,6 @@
 let mix = require('laravel-mix')
 
-mix.js('resources/js/field.js', 'dist/js').vue()
+mix.js('resources/js/field.js', 'dist/js')
    .sass('resources/sass/field.scss', 'dist/css')
     .webpackConfig({
         resolve: {
