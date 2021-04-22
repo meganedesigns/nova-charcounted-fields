c851e5782465a13f68c56ab7e92a4834da5993d4
Nicholas Welch <nc.welch@icloud.com>
2021-04-21 20:40:44 -0400

    set maxlength attr

--- src/FieldCounted.php
+++ src/FieldCounted.php
@@ -6,11 +6,27 @@ use Laravel\Nova\Fields\Field;
 
 class FieldCounted extends Field
 {
+	private $pattern;
+	private $max;
+
 	public function maxChars (int $characters)
 	{
+		$this->max = $characters;
 		return $this->withMeta([
 			'maxChars' => $characters, 'extraAttributes' => [
-				'maxlength' => $characters
+				'maxlength' => $this->max,
+				'pattern' => $this->pattern
+			]
+		]);
+	}
+
+	public function pattern (string $pattern)
+	{
+		$this->pattern = $pattern;
+		return $this->withMeta([
+			'extraAttributes' => [
+				'maxlength' => $this->max,
+				'pattern' => $this->pattern
 			]
 		]);
 	}
